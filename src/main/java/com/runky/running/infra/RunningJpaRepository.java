2b285652b6f3067c4da3fbe4d7e016569188903a
plan11plan <tjtfy7028@gmail.com>
2025-08-25 03:33:47 +0900

    refactor(running): 러너Id get 쿼리 에러 수정 #71
    - @Query()로 Long값 가져오도록 수정

--- src/main/java/com/runky/running/infra/RunningJpaRepository.java
+++ src/main/java/com/runky/running/infra/RunningJpaRepository.java
@@ -1,31 +1,20 @@
 package com.runky.running.infra;
 
-import com.runky.running.domain.Running;
-import com.runky.running.domain.RunningInfo;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.runky.running.domain.Running;
+
 public interface RunningJpaRepository extends JpaRepository<Running, Long> {
-    boolean existsByRunnerIdAndEndedAtIsNull(Long runnerId);
+	boolean existsByRunnerIdAndEndedAtIsNull(Long runnerId);
 
-    Optional<Running> findByIdAndRunnerId(Long id, Long runnerId);
+	Optional<Running> findByIdAndRunnerId(Long id, Long runnerId);
 
-    @Query("SELECT new com.runky.running.domain.RunningInfo$RunningResult(r.runnerId, SUM(r.totalDistanceMeter)) " +
-            "FROM Running r " +
-            "WHERE r.status = com.runky.running.domain.Running.Status.FINISHED " +
-            "AND r.startedAt >= :from " +
-            "AND r.endedAt <= :to " +
-            "GROUP BY r.runnerId")
-    List<RunningInfo.RunningResult> findRunnerDistanceResultsByPeriod(
-            LocalDateTime from,
-            LocalDateTime to
-    );
-  
 	boolean existsByIdAndStatus(Long id, Running.Status status);
 
+	@Query("select r.runnerId from Running r where r.id = :id")
 	Optional<Long> findRunnerIdById(Long id);
 
 }

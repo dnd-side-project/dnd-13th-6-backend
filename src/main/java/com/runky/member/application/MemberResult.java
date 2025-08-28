e80d958c8b7f21e105d26dc4e99b051665f83743
plan11plan <tjtfy7028@gmail.com>
2025-08-28 21:10:28 +0900

    refactor(member): 유저 정보 조회에 뱃지 id 추가

--- src/main/java/com/runky/member/application/MemberResult.java
+++ src/main/java/com/runky/member/application/MemberResult.java
@@ -4,31 +4,33 @@ import com.runky.member.domain.Member;
 import com.runky.reward.domain.Badge;
 
 public record MemberResult(
-        Long id,
-        String role,
-        String nickname
+	Long id,
+	String role,
+	String nickname
 ) {
-    public static MemberResult from(Member member) {
-        return new MemberResult(
-                member.getId(),
-                member.getRole().name(),
-                member.getNickname().value()
-        );
-    }
+	public static MemberResult from(Member member) {
+		return new MemberResult(
+			member.getId(),
+			member.getRole().name(),
+			member.getNickname().value()
+		);
+	}
 
-    public record WithBadge(
-            Long id,
-            String role,
-            String nickname,
-            String badgeImageUrl
-    ) {
-        public static MemberResult.WithBadge of(Member member, Badge badge) {
-            return new MemberResult.WithBadge(
-                    member.getId(),
-                    member.getRole().name(),
-                    member.getNickname().value(),
-                    badge.getImageUrl()
-            );
-        }
-    }
+	public record WithBadge(
+		Long id,
+		String role,
+		String nickname,
+		Long badgeId,
+		String badgeImageUrl
+	) {
+		public static MemberResult.WithBadge of(Member member, Badge badge) {
+			return new MemberResult.WithBadge(
+				member.getId(),
+				member.getRole().name(),
+				member.getNickname().value(),
+				badge.getId(),
+				badge.getImageUrl()
+			);
+		}
+	}
 }

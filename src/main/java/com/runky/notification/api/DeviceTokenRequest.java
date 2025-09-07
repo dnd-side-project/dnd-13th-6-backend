6d7f2162c1d5ae4d77eb8c774a0809fee09ae728
plan11plan <tjtfy7028@gmail.com>
2025-09-08 00:47:02 +0900

    refactor: controlelr Dto 스코프 public으로 변경

--- src/main/java/com/runky/notification/api/DeviceTokenRequest.java
+++ src/main/java/com/runky/notification/api/DeviceTokenRequest.java
@@ -5,13 +5,13 @@ import com.runky.notification.application.NotificationCriteria;
 public sealed interface DeviceTokenRequest {
 
 	record Register(String token, String deviceType) implements DeviceTokenRequest {
-		NotificationCriteria.RegisterDeviceToken toCriteria(Long memberId) {
+		public NotificationCriteria.RegisterDeviceToken toCriteria(Long memberId) {
 			return new NotificationCriteria.RegisterDeviceToken(memberId, token, deviceType);
 		}
 	}
 
 	record Delete(String token) implements DeviceTokenRequest {
-		NotificationCriteria.DeleteDeviceToken toCriteria(Long memberId) {
+		public NotificationCriteria.DeleteDeviceToken toCriteria(Long memberId) {
 			return new NotificationCriteria.DeleteDeviceToken(memberId, token);
 
 		}

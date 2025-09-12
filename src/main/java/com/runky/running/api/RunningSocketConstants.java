39efeb8134eac2bf0d7c092f78663d5d95a8831e
plan11plan <tjtfy7028@gmail.com>
2025-09-12 20:49:50 +0900

    feat: 런닝 웹소캣 상수 클래스 추가

---
+++ src/main/java/com/runky/running/api/RunningSocketConstants.java
@@ -0,0 +1,8 @@
+package com.runky.running.api;
+
+public class RunningSocketConstants {
+	public static final String ATTR_MEMBER_ID = "memberId"; // ★ 핸들러와 동일 키
+	public static final String AUTHORIZATION = "Authorization";
+	public static final String BEARER_ = "Bearer ";
+
+}

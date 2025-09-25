086326180272e8cf52caef39f3f5e1ca3dd38326
hgh1472 <hgh1472@naver.com>
2025-09-25 21:42:11 +0900

    fix: 쿼리 공백 수정

--- src/main/java/com/runky/goal/domain/batch/CrewGoalReader.java
+++ src/main/java/com/runky/goal/domain/batch/CrewGoalReader.java
@@ -8,7 +8,7 @@ public class CrewGoalReader extends JpaPagingItemReader<CrewGoalSum> implements 
     public CrewGoalReader(EntityManagerFactory emf) {
         super();
         setEntityManagerFactory(emf);
-        setQueryString("SELECT new com.runky.goal.domain.batch.CrewGoalSum(cm.crew.id, SUM(mg.goal.value))" +
+        setQueryString("SELECT new com.runky.goal.domain.batch.CrewGoalSum(cm.crew.id, SUM(mg.goal.value)) " +
                 "FROM CrewMember cm " +
                 "JOIN MemberGoal mg ON cm.memberId = mg.memberId " +
                 "WHERE cm.role IN (com.runky.crew.domain.CrewMember.Role.MEMBER, com.runky.crew.domain.CrewMember.Role.LEADER) " +

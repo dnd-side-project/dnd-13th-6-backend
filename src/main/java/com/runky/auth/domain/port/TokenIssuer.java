3de3b9eb50874c3e3fb40bb20b138fd86da2123a
plan11plan <tjtfy7028@gmail.com>
2025-10-15 18:39:45 +0900

    chore(Auth): 인증 구조 재설계로 인한 불필요한 코드 삭제

--- src/main/java/com/runky/auth/domain/port/TokenIssuer.java
+++
@@ -1,8 +0,0 @@
-package com.runky.auth.domain.port;
-
-import com.runky.auth.domain.vo.IssuedTokens;
-
-public interface TokenIssuer {
-	IssuedTokens issue(Long memberId, String memberRole);
-
-}
--- src/main/java/com/runky/auth/domain/vo/RawAccessToken.java
+++
@@ -1,6 +0,0 @@
-package com.runky.auth.domain.vo;
-
-import java.time.Instant;
-
-public record RawAccessToken(String token, Instant issuedAt, Instant expiresAt) {
-}
